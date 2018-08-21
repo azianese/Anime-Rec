c8bdb790662b21b635bba17e55ac245f8f0e61a1
Azianese <sam.wang.279x@gmail.com>
2018-08-21 13:50:03 -0700

    finished about half of the input class

--- Input.java
+++ Input.java
@@ -3,51 +3,63 @@ import java.util.*;
 
 public class Input {
 
+  //basic data
   int rating_weight;
   int min_votes;
   int max_votes;
   int min_year;
   int max_year;
+  //specific data
   int genres_weight;
-  List<String> genres;
+  List<String> genres = new ArrayList<String>();
   int themes_weight;
-  List<String> themes;
+  List<String> themes = new ArrayList<String>();
   int director_weight;
   String director;
   int studio_weight;
   String studio;
-  
+  //other data
   int numRecs;
   Boolean compareAnime;
 
   //Default costructor
-  public Input() {
-    if (getDefault().equals("y")) {
+  public Input(Scanner sc, List<Anime> animeList) {
+    if (getDefault(sc).equals("y")) {
       setDefault();
       return;
     }
-    numRecs = getNumRecs();
-    if (getPurpose().equals("y")) {
-      
-    }
-    else {
-      
+    numRecs = getNumRecs(sc);
+    rating_weight = getRatingWeight(sc);
+    min_votes = getMinVotes(sc);
+    max_votes = getMaxVotes(sc);
+    min_year = getMinYear(sc);
+    max_year = getMaxYear(sc);
+
+    if (getPurpose(sc)) 
+      compareAnime = useAnime(sc, animeList);
+    else
+      compareAnime = false;
+    if (!compareAnime) {
+      genres_weight = getGenresWeight(sc);
+      genres = getGenres(sc);
+      themes_weight = getThemesWeight(sc);
+      themes = getThemes(sc);
+      director_weight = getDirectorWeight(sc);
+      director = getDirector(sc);
+      studio_weight = getStudioWeight(sc);
+      studio = getStudio(sc);
     }
   }
 
-
-
-
   //Asks for default recommendations
-  private String getDefault() {
-    System.out.println("Do you want to print the default recommendations? (y/n)?");
-    Scanner reader = new Scanner(System.in);
-    String s = reader.nextLine();
-    if (!s.equalsIgnoreCase("y") && !s.equalsIgnoreCase("n")) {
+  private String getDefault(Scanner sc) {
+    System.out.println("Do you want the default recommendations? (y/n)?");
+    String s = sc.nextLine();
+    while (!s.equalsIgnoreCase("y") && !s.equalsIgnoreCase("n")) {
       System.out.println("Sorry, invalid input. only 'y' or 'n' are valid inputs.");
-      s = getDefault();
+      System.out.println("Do you want the default recommendations? (y/n)?");
+      s = sc.nextLine();
     }
-    reader.close();
     return s;
   }
 
@@ -67,43 +79,162 @@ public class Input {
     this.director = "";
     this.studio_weight = 0;
     this.studio = "";
-    
+
     this.numRecs = 100;
     this.compareAnime = false;
   }
 
   //Asks for the number of recommendations
-  private int getNumRecs() {
+  private int getNumRecs(Scanner sc) {
     System.out.println("How many anime recommendations do you want (up to 500)?");
-    Scanner reader = new Scanner(System.in);
-    String s = reader.nextLine();
+    String s = sc.nextLine();
     int num = 0;
-    try {
-      num = Integer.valueOf(s);
-      if (0 > num || num > 500) {
+    boolean valid = false;
+    while (!valid) {
+      try {
+        num = Integer.valueOf(s);
+        if (0 > num || num > 500) {
+          System.out.println("Sorry, invalid input. Only numbers between 0 and 500 [inclusive] are allowed.");
+          System.out.println("How many anime recommendations do you want (up to 500)?");
+          s = sc.nextLine();
+        }
+        else
+          valid = true;
+      }
+      catch (Exception e) {
         System.out.println("Sorry, invalid input. Only numbers between 0 and 500 [inclusive] are allowed.");
-        num = getNumRecs();
+        System.out.println("How many anime recommendations do you want (up to 500)?");
+        s = sc.nextLine();
       }
     }
-    catch (Exception e) {
-      System.out.println("Sorry, invalid input. Only numbers between 0 and 500 [inclusive] are allowed.");
-      num = getNumRecs();
-    }
-    reader.close();
     return num;
   }
 
+  private int getRatingWeight(Scanner sc) {
+    System.out.println("How much do you value the anime's rating (1-10)?");
+    String s = sc.nextLine();
+    int num = 0;
+    boolean valid = false;
+    while (!valid) {
+      try {
+        num = Integer.valueOf(s);
+        if (num < 0 || num > 10) {
+          System.out.println("Sorry, invalid input. Only whole numbers between 0 and 10 [inclusive] are allowed.");
+          System.out.println("How much do you value the anime's rating (1-10)?");
+          s = sc.nextLine();
+        }
+        else
+          valid = true;
+      }
+      catch (Exception e) {
+        System.out.println("Sorry, invalid input. Only whole numbers between 0 and 10 [inclusive] are allowed.");
+        System.out.println("How much do you value the anime's rating (1-10)?");
+        s = sc.nextLine();
+      }
+    }
+    return num;
+  }
+
+  private int getMinVotes(Scanner sc) {
+    return 0;
+  }
+
+  private int getMaxVotes(Scanner sc) {
+    return 0;
+  }
+
+  private int getMinYear(Scanner sc) {
+    return 0;
+  }
+
+  private int getMaxYear(Scanner sc) {
+    return 0;
+  }
+
   //Asks if the user wants to use a single anime for recomendations
-  private String getPurpose() {
+  private Boolean getPurpose(Scanner sc) {
     System.out.println("Do you have an anime in mind that you want to find similar animes to? (y/n)");
-    Scanner reader = new Scanner(System.in);
-    String s = reader.nextLine();    
-    if (!s.equalsIgnoreCase("y") && !s.equalsIgnoreCase("n")) {
+    String s = sc.nextLine();
+    while (!s.equalsIgnoreCase("y") && !s.equalsIgnoreCase("n")) {
       System.out.println("Sorry, invalid input. only 'y' or 'n' are valid inputs.");
-      s = getPurpose();
+      System.out.println("Do you have an anime in mind that you want to find similar animes to? (y/n)");
+      s = sc.nextLine();
     } 
-    reader.close();
-    return s;
+    if (s.equalsIgnoreCase("y")) 
+      return true;
+    else 
+      return false;
+  }
+
+  private Boolean useAnime(Scanner sc, List<Anime> animeList) {
+    System.out.println("What anime would you like to use as a reference?");
+    String animeName = sc.nextLine();
+    boolean animeFound = false;
+    int i = 0;
+    for (; i < animeList.size(); ++i) {
+      if (animeList.get(i).name.equalsIgnoreCase(animeName)) {
+        animeFound = true;
+        break;
+      }
+    }
+    if (animeFound == false) {
+      System.out.println("Sorry that anime was not found. Would you like to try a different anime (y/n)?");
+      String s = sc.nextLine();
+      while (!s.equalsIgnoreCase("y") && !s.equalsIgnoreCase("n")) {
+        System.out.println("Sorry, invalid input. only 'y' or 'n' are valid inputs.");
+        System.out.println("Would you like to try a different anime (y/n)?");
+        s = sc.nextLine();
+      }
+      if (s.equals("y")) {
+        return useAnime(sc, animeList);
+      }
+      else {
+        return false;
+      }
+    }
+    genres_weight = 10;
+    for (String genre : animeList.get(i).genres)
+      genres.add(genre);    
+    themes_weight = 10;
+    for (String theme : animeList.get(i).themes)
+      themes.add(theme);    
+    director_weight = 5;    
+    director = animeList.get(i).director;
+    studio_weight = 5;
+    studio = animeList.get(i).studio;
+    return true;
+  }
+
+  private int getGenresWeight(Scanner sc) {
+    return 0;
+  }
+
+  private List<String> getGenres(Scanner sc) {
+    return new ArrayList<String>();
+  }
+
+  private int getThemesWeight(Scanner sc) {
+    return 0;
+  }
+
+  private List<String> getThemes(Scanner sc) {
+    return new ArrayList<String>();
+  }
+
+  private int getDirectorWeight(Scanner sc) {
+    return 0;
+  }
+
+  private String getDirector(Scanner sc) {
+    return "";
+  }
+
+  private int getStudioWeight(Scanner sc) {
+    return 0;
+  }
+
+  private String getStudio(Scanner sc) {
+    return "";
   }
 
 }
--- Main.java
+++ Main.java
@@ -4,77 +4,32 @@ import java.util.*;
 public class Main {
 
   public static void main(String[] args) throws IOException{
-    
-    ///////////////// TESTING AREA /////////////////
 
-    //setup to determine significance of each category with respect to anime score
-    /*
-    int rating_weight = 0;
-    int min_votes = 0;
-    int max_votes = 9999;
-    
-    // ((genre score)/total number of genres) * matched genres
-    int genres_weight = 0;
-    // (themes score)/total number of genres) * matched themes
-    int themes_weight = 10;
-    
-    int director_weight = 0;
-    int studio_weight = 0;
-    int min_year = 0;
-    int max_year = 2020;
-    
-    List<String> genres = new ArrayList<>(Arrays.asList("adventure"));
-    List<String> themes = new ArrayList<>(Arrays.asList("alternate history","conspiracy","immortality","mecha","military","mind control","politics","real robot","revenge","strategic minds","terrorism","tragedy","war"));
-    String director = "goro taniguchi";
-    String studio = "Bandai Co., Ltd.";
-    */
-    Input input = new Input();
-    
-    int rating_weight = input.rating_weight;
-    int min_votes = input.min_votes;
-    int max_votes = input.max_votes;
-    
-    // ((genre score)/total number of genres) * matched genres
-    int genres_weight = input.genres_weight;
-    // (themes score)/total number of genres) * matched themes
-    int themes_weight = input.themes_weight;
-    
-    int director_weight = input.director_weight;
-    int studio_weight = input.studio_weight;
-    int min_year = input.min_year;
-    int max_year = input.max_year;
-    
-    List<String> genres = input.genres;
-    List<String> themes = input.themes;
-    String director = input.director;
-    String studio = input.studio;
-    
-    ///////////////// END TESTING AREA /////////////////
-    
-    PriorityQueue<Anime> pq = new PriorityQueue<Anime>(new AnimeComparator());
-    
-    //starts to read the file, AnimeData.txt
+    List<Anime> animeList = new ArrayList<Anime>();
+
+    //starts to read the file, AnimeData.txt.
+    //then adds anime to a list of anime.
     try (BufferedReader br = new BufferedReader(new FileReader("AnimeData.txt"))) {
 
       //skips the first 3 useless lines in the AnimeData file
       String line = "";
       for (int i = 0; i < 3; ++i)
         line = br.readLine();
-      
+
       //until the end of the file, gets the first line of each Anime
       while (line != null) {
         line = br.readLine();
         StringBuilder sb = new StringBuilder(line);
         int start = sb.indexOf(":") + 2;
-        
+
         //Creates a new anime object for the current anime
         Anime anime = new Anime();
         anime.name = sb.substring(start);
-        
+
         //reads lines of data for this particular anime
         while ((line = br.readLine()) != null) {
           StringBuilder sb2 = new StringBuilder(line);
-      
+
           //breaks the loop if a colon isn't found, indicating the end of this anime's data
           start = sb2.indexOf(":") + 2;
           if (start < 2) break;
@@ -125,35 +80,15 @@ public class Main {
           else if (sb2.indexOf("production studio") != -1) {
             anime.studio = data;
           }
-          
+
           //does nothing with any other data
           else {
             //System.out.println(line);
           }
         }//end of data for this anime
-        
-        anime.score = anime.determineScore(rating_weight, 
-                                           min_votes, max_votes, 
-                                           genres, genres_weight, 
-                                           themes, themes_weight, 
-                                           director, director_weight, 
-                                           studio, studio_weight, 
-                                           min_year, max_year);
-        
-        ///////////////// TESTING AREA /////////////////
-        
-        if(anime.score > 3) {
-          System.out.println("-----------------" + anime.ranking);
-          System.out.println(anime.name);
-          System.out.println("score: " + anime.score);
-          /*
-          for (int i = 0; i < anime.themes.length; ++i) {
-            System.out.println(anime.themes[i]);
-          }*/
-        }
-        
-        ///////////////// END TESTING AREA /////////////////
-        
+
+        animeList.add(anime);
+
       }//file has no more lines to read
     }
     /*
@@ -161,6 +96,45 @@ public class Main {
       System.out.println("Oops. Something went wrong");
       System.out.println(e);
     }*/
+
+    ///////////////// TESTING AREA /////////////////
+
+    System.out.println("anime list size: " + animeList.size());
+    Scanner sc = new Scanner(System.in);
+    Input input = new Input(sc, animeList);
+    PriorityQueue<Anime> pq = new PriorityQueue<Anime>(new AnimeComparator());
+
+    for (int i = 0; i < animeList.size(); ++i) {
+      Anime anime = animeList.get(i);
+      anime.score = anime.determineScore(input.rating_weight,
+                                         input.min_votes, input.max_votes, 
+                                         input.genres, input.genres_weight, 
+                                         input.themes, input.themes_weight, 
+                                         input.director, input.director_weight, 
+                                         input.studio, input.studio_weight, 
+                                         input.min_year, input.max_year);
+      pq.add(anime);
+    }
+
+    for (int i = 1; i < 11; ++i) {
+      System.out.print(i + ": ");
+      System.out.println(pq.poll().name);
+    }
+
+    /*
+    if(anime.score > 3) {
+      System.out.println("-----------------" + anime.ranking);
+      System.out.println(anime.name);
+      System.out.println("score: " + anime.score);
+    }
+
+    for (int i = 0; i < anime.themes.length; ++i) {
+      System.out.println(anime.themes[i]);
+    }
+    */
+
+    sc.close();
+    ///////////////// END TESTING AREA /////////////////
   }
 }
 
