211be83160e9fa216ceab3f11b268b0e2c0bef31
Azianese <sam.wang.279x@gmail.com>
2018-10-28 12:44:42 -0700

    edited data .txt files

--- MySQL/tester.sql
+++ MySQL/tester.sql
@@ -1,2 +1,7 @@
-USE nirzj0shn94smawo;anime
+USE nirzj0shn94smawo;
 
+LOAD DATA LOCAL INFILE '/mySQL/AnimeTable.txt' INTO TABLE anime;
+LOAD DATA LOCAL INFILE '/mySQL/DirectorsTable.txt' INTO TABLE directors;
+LOAD DATA LOCAL INFILE '/mySQL/StudiosTable.txt' INTO TABLE studios;
+LOAD DATA LOCAL INFILE '/mySQL/GenresTable.txt' INTO TABLE genres;
+LOAD DATA LOCAL INFILE '/mySQL/ThemesTable.txt' INTO TABLE themes;
