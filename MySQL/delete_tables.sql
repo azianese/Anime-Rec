8c73be91d36f1aa1831be01745ada69992317308
Azianese <sam.wang.279x@gmail.com>
2018-11-11 17:59:14 -0800

    minor file changes

--- MySQL/delete_tables.sql
+++ MySQL/delete_tables.sql
@@ -1,6 +1,15 @@
 USE nirzj0shn94smawo;
-DROP TABLE IF EXISTS anime_has_genres;
-DROP TABLE IF EXISTS anime_has_themes;
+
+DROP TABLE IF EXISTS anime_to_genres;
+DROP TABLE IF EXISTS anime_to_themes;
+DROP TABLE IF EXISTS anime_to_directors;
+DROP TABLE IF EXISTS anime_to_studios;
+
+DROP TABLE IF EXISTS anime_directors;
+DROP TABLE IF EXISTS anime_studios;
+DROP TABLE IF EXISTS anime_genres;
+DROP TABLE IF EXISTS anime_themes;
+
 DROP TABLE IF EXISTS anime;
 DROP TABLE IF EXISTS directors;
 DROP TABLE IF EXISTS studios;
